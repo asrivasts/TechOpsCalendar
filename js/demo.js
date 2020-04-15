b779a523b5d78f2c9ffc6236529e265a6a8ebe81
Anu Srivastava <asrivasts@gmail.com>
2020-04-15 09:57:40 -0400

    calendar

---
+++ js/demo.js
@@ -0,0 +1,12 @@
+var events = [
+  {'Date': new Date(2020, 3, 2), 'Title': 'VMware - Cloud Management  VM Tools: 11AM - 12PM'},
+  {'Date': new Date(2020, 3, 9), 'Title': 'VMware: vSphere 7.0 and SDDC: 12PM - 1PM'},
+  {'Date': new Date(2020, 3, 16), 'Title': 'VMware: VMC on AWS and Hybrid Cloud: 11AM - 12PM'},
+  {'Date': new Date(2020, 3, 23), 'Title': 'Midrange .NEXT: 11AM - 12PM'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
+
+
+// {'Date': new Date(2020, 4, 16), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
