1ddc218b4506cd006f6d92d5ed1c115dc207e583
Anu Srivastava <asrivasts@gmail.com>
2020-04-20 13:22:57 -0400

    Shifted dates

--- js/demo.js
+++ js/demo.js
@@ -4,7 +4,7 @@ var events = [
   {'Date': new Date(2020, 3, 16), 'Title': 'VMware: VMC on AWS and Hybrid Cloud: 11AM - 12PM'},
   {'Date': new Date(2020, 3, 23), 'Title': 'Midrange .NEXT: 11AM - 12PM'},
   {'Date': new Date(2020, 3, 30), 'Title': "Dell PowerOne - Dell's New Converged Platform: 11AM - 12PM"}
-  {'Date': new Date(2020, 3, 30), 'Title': 'SD-WAN Edge Powered by VeloCloud: 11AM - 12PM', 'Link' : 'https://www.delltechnologies.com/en-us/video-collateral/sd-wan-edge-powered-by-velocloud.htm'}
+  {'Date': new Date(2020, 4, 7), 'Title': 'SD-WAN Edge Powered by VeloCloud: 11AM - 12PM', 'Link' : 'https://www.delltechnologies.com/en-us/video-collateral/sd-wan-edge-powered-by-velocloud.htm'}
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
