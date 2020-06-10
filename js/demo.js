85891deca2bfd345cdfb22b935dc563cc45fda96
Anu Srivastava <asrivasts@gmail.com>
2020-06-10 09:09:46 -0400

    June updates

--- js/demo.js
+++ js/demo.js
@@ -6,12 +6,11 @@ var events = [
   {'Date': new Date(2020, 3, 30), 'Title': "Dell PowerOne - Dell's New Converged Platform: 11AM - 12PM"},
   {'Date': new Date(2020, 4, 7), 'Title': 'SD-WAN Edge Powered by VeloCloud: 11AM - 12PM', 'Link' : 'https://www.delltechnologies.com/en-us/video-collateral/sd-wan-edge-powered-by-velocloud.htm'},
   {'Date': new Date(2020, 4, 14), 'Title': 'Data Protection and Resilliency: 11AM - 1PM'},
-  {'Date': new Date(2020, 4, 21), 'Title': 'Boomi - Cloud Integration Platform: 11AM - 12PM'},
-  {'Date': new Date(2020, 4, 28), 'Title': 'Unstructured Data Briefing: 11AM - 12PM'}
+  {'Date': new Date(2020, 4, 21), 'Title': 'Monthly Business Review: 11AM - 12PM'},
+  {'Date': new Date(2020, 4, 28), 'Title': 'Unstructured Data Briefing: 11AM - 12PM'},
+  {'Date': new Date(2020, 5, 03), 'Title': 'Review of Edu Sessions (POC?): 11AM - 12PM'},
+  {'Date': new Date(2020, 5, 18), 'Title': 'Microsoft Services: 11AM - 12PM'}
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
 caleandar(element, events, settings);
-
-
-// {'Date': new Date(2020, 4, 16), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
