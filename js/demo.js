b726f78f14043092f314c1b4ae8c9a1339bf3a99
Anu Srivastava <asrivasts@gmail.com>
2020-06-10 09:11:52 -0400

    June updates

--- js/demo.js
+++ js/demo.js
@@ -9,7 +9,7 @@ var events = [
   {'Date': new Date(2020, 4, 21), 'Title': 'Monthly Business Review: 11AM - 12PM'},
   {'Date': new Date(2020, 4, 28), 'Title': 'Unstructured Data Briefing: 11AM - 12PM'},
   {'Date': new Date(2020, 5, 04), 'Title': 'Review of Edu Sessions (POC?): 11AM - 12PM'},
-  {'Date': new Date(2020, 5, 18), 'Title': 'Microsoft Services: 11AM - 12PM'}
+  {'Date': new Date(2020, 5, 18), 'Title': 'Microsoft 365 Services: 11AM - 12PM'}
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
